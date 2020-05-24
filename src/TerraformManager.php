54e176839090627d039abeea8bd424598b633c56
sdwru <40401953+sdwru@users.noreply.github.com>
2020-05-23 19:12:13 -0700

    Update TerraformManager.php

--- src/TerraformManager.php
+++ src/TerraformManager.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Sdwru\Terraform;
+namespace sdwru\Terraform;
 
 use GrahamCampbell\Manager\AbstractManager;
 use Illuminate\Contracts\Config\Repository;
