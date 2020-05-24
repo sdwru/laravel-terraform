3f0ffcf1a7d49a9084c33dee7cf4bf9311c2c368
sdwru <40401953+sdwru@users.noreply.github.com>
2020-05-23 19:13:18 -0700

    Update GuzzleConnector.php

--- src/Adapter/Connector/GuzzleConnector.php
+++ src/Adapter/Connector/GuzzleConnector.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Sdwru\Terraform\Adapter\Connector;
+namespace sdwru\Terraform\Adapter\Connector;
 
 use TerraformV2\Adapter\GuzzleHttpAdapter;
 use GrahamCampbell\Manager\ConnectorInterface;
